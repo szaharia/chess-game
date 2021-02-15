8bbc8de00813ebfb06d5caaebcca9c7aea73c4f6
Silviu Zaharia <scorpion1195@yahoo.com>
2021-02-15 03:11:04 +0200

    Added some missing files

---
+++ ChessGame/ChessGame.Business/InternalClasses/UndeleteablePlayerException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.Business.InternalClasses
+{
+    public class UndeleteablePlayerException: Exception
+    {
+        public UndeleteablePlayerException(string exception):base(exception)
+        {
+
+        }
+    }
+}
